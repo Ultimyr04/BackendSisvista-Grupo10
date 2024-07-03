813b10ecfc9809e89bd2427edd18bac6658df5a6
rodrigoYamil <curanvolk.99@gmail.com>
2024-07-02 22:46:20 -0500

    Guardar cambios locales antes de pull

--- models/test_puntaje.py
+++ models/test_puntaje.py
@@ -6,12 +6,14 @@ class TestPuntaje(db.Model):
     totaltest1 = db.Column(db.Integer)
     totaltest2 = db.Column(db.Integer)
     totaltest3 = db.Column(db.Integer)
+    promedio = db.Columns(db.Integer)
     idnivelansiedad = db.Column(db.Integer, db.ForeignKey('nivelansiedad.idnivelansiedad'))
     idperfil = db.Column(db.Integer, db.ForeignKey('perfilusuario.idperfil'))
 
-    def __init__(self, totaltest1=None, totaltest2=None, totaltest3=None, idnivelansiedad=None, idperfil=None):
+    def __init__(self, totaltest1=None, totaltest2=None, totaltest3=None, promedio=None , idnivelansiedad=None, idperfil=None):
         self.totaltest1 = totaltest1
-        self.totaltest2 = None
-        self.totaltest3 = None
+        self.totaltest2 = totaltest2
+        self.totaltest3 = totaltest3
         self.idnivelansiedad = idnivelansiedad
         self.idperfil = idperfil
+        self.promedio = promedio
