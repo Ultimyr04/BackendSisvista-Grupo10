9948cf28f66c26250cf9243378967d94e3016ded
rodrigoYamil <curanvolk.99@gmail.com>
2024-07-03 00:16:55 -0500

    Cambios agregados

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -8,19 +8,16 @@ from routes.test_puntaje_routes import test_puntaje_routes
 from routes.perfil_usuario_routes import perfil_usuario_routes
 from routes.test_respuesta_routes import test_routes
 from routes.mapacalor_routes import mapa_calor
-from config import Config
-from utils.db import db
-from routes.test_db import test_db_bp
-from routes.user_routes import user_bp  # Asegúrate de importar el blueprint correctamente
+from routes.user_routes import user_bp
 from routes.register_routes import register_bp
 from config import Config
 from utils.db import db
+from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.exceptions import HTTPException
 
-
-
+# Configurar registro de errores
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -31,7 +28,7 @@ app.config["SQLALCHEMY_POOL_SIZE"] = 20
 app.config["SQLALCHEMY_POOL_TIMEOUT"] = 30
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 1800
 
-# no cache
+# No cachear archivos
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 db.init_app(app)
@@ -39,13 +36,37 @@ jwt = JWTManager(app)
 
 app.secret_key = 'clavesecreta123'
 
+# Registra los blueprints
 app.register_blueprint(test_db_bp)
-app.register_blueprint(user_bp)  # Registra el blueprint
+app.register_blueprint(user_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(test_puntaje_routes)
 app.register_blueprint(test_routes)
 app.register_blueprint(perfil_usuario_routes)
 app.register_blueprint(mapa_calor)
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    """Maneja todas las excepciones no controladas."""
+    logger.error(f"An exception occurred: {str(e)}")
+    return jsonify(error=str(e)), 500
+
+
+@app.errorhandler(SQLAlchemyError)
+def handle_database_error(e):
+    """Maneja los errores de SQLAlchemy."""
+    logger.error(f"A database error occurred: {str(e)}")
+    db.session.rollback()  # Rollback en caso de error de base de datos
+    return jsonify(error="Database error"), 500
+
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(e):
+    """Maneja las excepciones HTTP."""
+    logger.error(f"An HTTP error occurred: {str(e)}")
+    return jsonify(error=str(e)), e.code
+
 
 if __name__ == '__main__':
     app.run(port=5000)
