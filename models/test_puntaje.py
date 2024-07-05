5048ee34deb5d51e7241b2be5e24528ae5075f40
Ultimyr04 <josegonzalesc04@gmail.com>
2024-07-05 03:16:12 -0500

    refinamiento de testpuntaje 3

--- models/test_puntaje.py
+++ models/test_puntaje.py
@@ -10,7 +10,7 @@ class TestPuntaje(db.Model):
     promedio = db.Column(db.Integer)
     idnivelansiedad = db.Column(db.Integer, db.ForeignKey('nivelansiedad.idnivelansiedad'))
     idperfil = db.Column(db.Integer, db.ForeignKey('perfilusuario.idperfil'))
-    fechatest = db.Column(db.Datetime, default = datetime.utcnow)
+    fechatest = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __init__(self, totaltest1=None, totaltest2=None, totaltest3=None, promedio=None, idnivelansiedad=None, idperfil=None):
         self.totaltest1 = totaltest1
